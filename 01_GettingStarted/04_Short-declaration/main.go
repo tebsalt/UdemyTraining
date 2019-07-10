d539f27b8561c3d3e526b46f9154a6cf4d009d34
Tebsalt <emmatebes@yahoo.com>
2019-07-10 15:16:56 +0100

    God is Gracious

---
+++ 01_GettingStarted/04_Short-declaration/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	fmt.Println(x)
+	x = 99
+	fmt.Println(x)
+	y := 100 + 24
+	z := "gogo"
+	fmt.Println(x, y, z)
+	p := 45
+	x = 7
+	fmt.Println(p, x)
+
+}
---
+++ tempONE/testing.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func () {
+  x:=42
+  fmt.Println(x)
+
+}
